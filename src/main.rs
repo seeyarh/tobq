1bbb5a18364ad862be0645ee5f0ebf25067378d3
Collins Huff <collinsrhuff@gmail.com>
2021-07-14 16:38:02 -0400

    add logging, fix timeout bug

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ use gcp_bigquery_client::model::table_data_insert_all_request::TableDataInsertAl
 use clap::Clap;
 
 use tokio::io::{stdin, AsyncBufReadExt, BufReader};
-use tokio::time::Instant;
+use tokio::time::{sleep, Duration, Instant};
 use tokio_stream::{wrappers::LinesStream, StreamExt};
+use tracing::info;
+use tracing_subscriber;
 
 /// Read json lines from stdin, write to bigquery
 #[derive(Debug, Clone, Clap)]
@@ -31,6 +33,8 @@ struct Opts {
 #[tokio::main]
 async fn main() -> Result<(), BQError> {
     let opts: Opts = Opts::parse();
+    tracing_subscriber::fmt::init();
+
     let project_id = &opts.project;
     let dataset_id = &opts.dataset;
     let table_id = &opts.table;
@@ -44,21 +48,44 @@ async fn main() -> Result<(), BQError> {
     let mut insert_request = TableDataInsertAllRequest::new();
     let mut cur_batch_size = 0;
 
-    while let Some(line) = stream.next().await {
-        let line = line?;
-        let row: Value = serde_json::from_str(&line)?;
-        insert_request.add_row(None, row)?;
-        cur_batch_size += 1;
+    let timeout = Duration::from_secs(opts.timeout);
+    loop {
+        tokio::select! {
+            line = stream.next() => {
+                let line = match line {
+                    Some(line) => line,
+                    None => break,
+                };
+                let line = line?;
+                let row: Value = serde_json::from_str(&line)?;
+                insert_request.add_row(None, row)?;
+                cur_batch_size += 1;
 
-        if start.elapsed().as_secs() > opts.timeout || cur_batch_size >= opts.batch_size {
-            client
-                .tabledata()
-                .insert_all(project_id, dataset_id, table_id, insert_request)
-                .await?;
+                if start.elapsed().as_secs() > opts.timeout || cur_batch_size >= opts.batch_size {
+                    info!(cur_batch_size, "calling insert with batch size");
+                    client
+                        .tabledata()
+                        .insert_all(project_id, dataset_id, table_id, insert_request)
+                        .await?;
+                    info!(cur_batch_size, "finished calling insert with batch size");
 
-            start = Instant::now();
-            insert_request = TableDataInsertAllRequest::new();
-            cur_batch_size = 0;
+                    start = Instant::now();
+                    insert_request = TableDataInsertAllRequest::new();
+                    cur_batch_size = 0;
+                }
+            },
+            _ = sleep(timeout) => {
+                    info!(cur_batch_size, "calling insert with batch size after timeout");
+                    client
+                        .tabledata()
+                        .insert_all(project_id, dataset_id, table_id, insert_request)
+                        .await?;
+                    info!(cur_batch_size, "finished calling insert with batch size");
+
+                    start = Instant::now();
+                    insert_request = TableDataInsertAllRequest::new();
+                    cur_batch_size = 0;
+            },
         }
     }
 
